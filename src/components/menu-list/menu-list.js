aee45ab99695e9a6dd2dbe56eb5becd82a9b8ccb
Artur <kovalskartur@yahoo.com>
2020-11-30 18:08:29 +0200

    'four'

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
 import MenuListItem from '../menu-list-item';
-
+import {connect} from 'react-redux'
+import WithRestoService from '../hoc'
 import './menu-list.scss';
 
 class MenuList extends Component {
 
     render() {
+        const {menuItems} = this.props;
 
         return (
             <ul className="menu__list">
-                <MenuListItem/>
+                {
+                    menuItems.map((menuItem) => {//приходит массив, который перебираем и потом данные по каждому пункту меню(ресторанное) передаем в menu-list-item b там каждое по отдельности рендерим 
+                        return <MenuListItem key={menuItem.id} menuItem={menuItem}/>
+                    }) 
+                }
             </ul>
         )
     }
 };
 
+const mapStateToProps = (state) => {
+    return{
+        menuItems: state.menu
+    }
+}
 
-export default MenuList;
+export default WithRestoService()(connect(mapStateToProps)(MenuList));//композиция компонентов высшего порядка 
